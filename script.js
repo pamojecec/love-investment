c1bba02d05827943f1448e73cd8b2a18f8df1049
Prakhar Yadav <94420829+pamojecec@users.noreply.github.com>
2025-10-25 23:01:12 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+// Calculate and show the total investment
+const herTotal = 2647;
+const hisTotal = 2530;
+const total = herTotal + hisTotal;
+
+document.getElementById(
+  "grandTotal"
+).innerText = `Together we’ve invested ₹${total}/- in love, care, and happiness 💞`;
